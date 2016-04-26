e319df9e456c9c75b4a554374e4f6c0461e9dac7
Vasiliy Kotov <xMPGek@gmail.com>
2016-04-26 10:46:54 +0300

    Changed not worked on the latest version ORB-FREAK-HAMMING to ORB-ORB-HAMMING.

--- Chapter3_MarkerlessAR/src/PatternDetector.hpp
+++ Chapter3_MarkerlessAR/src/PatternDetector.hpp
@@ -17,7 +17,8 @@
 #include "Pattern.hpp"
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/features2d.hpp>
+#include <opencv2/xfeatures2d.hpp>
 
 class PatternDetector
 {
@@ -27,9 +28,12 @@ public:
      */
     PatternDetector
         (
-        cv::Ptr<cv::FeatureDetector>     detector  = new cv::ORB(1000), 
-        cv::Ptr<cv::DescriptorExtractor> extractor = new cv::FREAK(false, false), 
-        cv::Ptr<cv::DescriptorMatcher>   matcher   = new cv::BFMatcher(cv::NORM_HAMMING, true),
+		//cv::Ptr<cv::FeatureDetector>     detector = new cv::ORB(1000),
+		//cv::Ptr<cv::DescriptorExtractor> extractor = new cv::FREAK(false, false),
+		//cv::Ptr<cv::DescriptorMatcher>   matcher = new cv::BFMatcher(cv::NORM_HAMMING, true),
+		cv::Ptr<cv::FeatureDetector>     detector = cv::ORB::create(1000),
+		cv::Ptr<cv::DescriptorExtractor> extractor = cv::ORB::create(1000),
+		cv::Ptr<cv::DescriptorMatcher>   matcher   = cv::DescriptorMatcher::create("BruteForce-Hamming"),
         bool enableRatioTest                       = false
         );
 
